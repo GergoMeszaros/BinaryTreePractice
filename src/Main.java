16aaff84a8d5ed8a1c431b56fe4f8e08d90a26b3
gergo1212 <gergo.meszaros.codecool@gmail.com>
2021-05-15 08:58:12 +0200

    separated the creation and addElements

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(  createTree());
-        
+        BinaryTree bt = new BinaryTree();
+        addElementsToTree(bt);
+        System.out.println(bt);
+
     }
 
-    private static BinaryTree createTree(){
-        BinaryTree bt = new BinaryTree();
-        bt.add(6);
-        bt.add(4);
-        bt.add(8);
-        bt.add(3);
-        bt.add(5);
-        bt.add(7);
-        bt.add(9);
-
-        return bt;
+    private static void addElementsToTree(BinaryTree tree){
+        tree.add(6);
+        tree.add(4);
+        tree.add(8);
+        tree.add(3);
+        tree.add(5);
+        tree.add(7);
+        tree.add(9);
     }
 }
